fff058f0bb012735e9ffe14f37eae538bad99f5d
M-HASNAIN-ALI-SHAH <23-cs-51@students.uettaxila.edu.pk>
2025-06-09 20:41:51 +0500

    completed basic version

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,6 +1,7 @@
 const { getRecieverSocketId, io } = require("../config/socket");
 const Message = require("../models/message.model");
 const cloudinary = require("../config/cloudinary.js");
+const Chat = require("../models/chat.model.js");
 
 const sendMessage = async (req, res) => {
   try {
@@ -20,12 +21,23 @@ const sendMessage = async (req, res) => {
       text,
       image: imageUrl,
     });
-
     await newMessage.save();
+
+    const getChat = await Chat.findById(chatId);
+    if (!getChat) {
+      return res.status(404).json({ error: "Chat not found" });
+    }
+    getChat.lastMessage = newMessage.text;
+    getChat.updatedAt = new Date();
+    await getChat.save();
 
     const receiverSocketId = getRecieverSocketId(recieverId);
     if (receiverSocketId) {
-      io.to(receiverSocketId).emit("newMessages", newMessage);
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+      io.to(receiverSocketId).emit("chatUpdate", {
+        chatId: getChat._id,
+        lastMessage: newMessage.text,
+      });
     }
 
     res.status(200).json(newMessage);
